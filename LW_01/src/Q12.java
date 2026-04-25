cf1b928987d452f326a4babe4a281016505326de
sithija-ws <sithija.ws@gmail.com>
2026-04-25 17:48:40 +0530

    feat: Question 12 done

---
+++ LW_01/src/Q12.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+public class Q12 {
+    public static void main(String[] args) {
+        JFrame myWindow = new JFrame();
+        myWindow.setSize(300,200);
+        myWindow.setTitle("My First Frame");
+
+        myWindow.setLocation(100,50);
+        myWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        myWindow.setVisible(true);
+    }
+}
