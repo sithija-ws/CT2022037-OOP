f0651ee0059312e3d97e9d513771f78a0d8ad186
wolfyblue078 <wolfyblue078@gmail.com>
2026-04-25 12:08:19 +0530

    feat: Question 1 done

---
+++ LW_01/src/Q1.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class Q1 {
+    public static void main(String[] args) {
+        JFrame myWindow = new JFrame();
+        myWindow.setSize(800,600);
+        myWindow.setTitle("Welcome to Java");
+        myWindow.setVisible(true);
+    }
+}
