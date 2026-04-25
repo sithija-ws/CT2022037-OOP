a1eb79492527b0728a87e90f03253b0e16d8527a
sithija-ws <sithija.ws@gmail.com>
2026-04-25 17:13:34 +0530

    feat: Question 10 done

---
+++ LW_01/src/Q10.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Q10 {
+    public static void main(String[] args) {
+        Scanner myScanner = new Scanner(System.in);
+        String text;
+
+        //get input
+        System.out.print("Enter your text: ");
+        text = myScanner.nextLine();
+
+        int textLength = text.length();
+
+        if(textLength%2 == 1 || textLength != 1){
+            System.out.println(text.charAt((textLength/2)));
+        } else {
+            System.out.println("Invalid input!");
+        }
+    }
+}
